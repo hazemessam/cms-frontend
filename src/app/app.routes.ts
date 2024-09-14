a48d6fa5fa823406342343d9c73f0d61b0b48462
Hazem <hazemkwita123@gmail.com>
2024-09-15 02:10:15 +0300

    Fix import path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { EmployeesComponent } from './components/employees/employees.component';
-import { EmployeesGuard } from './guards/admin.guard';
+import { EmployeesGuard } from './guards/employees.guard';
 
 export const routes: Routes = [
   {
